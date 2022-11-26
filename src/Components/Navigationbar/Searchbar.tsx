58809a43cc84d6e257461d9c16f28f9dc0716e07
Henning <henning.koenigs@gmx.de>
2022-11-26 18:51:02 +0100

    improve seachbar functions
    add on enter search, add remove input button and set exercise null on new search

--- src/Components/Navigationbar/Searchbar.tsx
+++ src/Components/Navigationbar/Searchbar.tsx
@@ -1,20 +1,36 @@
+import CloseIcon from "@mui/icons-material/Close"
 import SearchIcon from "@mui/icons-material/Search"
 import { Divider, IconButton, InputBase } from "@mui/material"
 import * as React from "react"
+import { exerciseActions } from "../../Store/State/ExerciseState"
 import { searchActions } from "../../Store/State/SearchState"
-import { useDispatch } from "../../Store/Store"
+import { useDispatch, useSelector } from "../../Store/Store"
 import { getExercises } from "../../Store/Thunks/getExercises"
 import "../../Styles/NavigationBar/SearchBar.scss"
 
 export const SearchBar: React.FC = () => {
   const dispatch = useDispatch()
+  const value = useSelector((state) => state.search.query)
 
   const onChange = (event: any) => {
     dispatch(searchActions.setQuery(event.target.value))
   }
 
-  const onClick = () => {
+  const onKeyUp = (
+    event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    if (event.key === "Enter") {
+      onClickSearch()
+    }
+  }
+
+  const onClickDelete = () => {
+    dispatch(searchActions.setQuery(""))
+  }
+
+  const onClickSearch = () => {
     dispatch(getExercises())
+    dispatch(exerciseActions.setExercise(null))
   }
 
   return (
@@ -24,13 +40,23 @@ export const SearchBar: React.FC = () => {
           className="InputBase"
           style={{ color: "white" }}
           onChange={onChange}
+          value={value}
+          onKeyUp={onKeyUp}
         />
+        <IconButton
+          type="button"
+          aria-label="close"
+          color="primary"
+          onClick={onClickDelete}
+        >
+          <CloseIcon />
+        </IconButton>
         <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <IconButton
           type="button"
           aria-label="search"
           color="primary"
-          onClick={onClick}
+          onClick={onClickSearch}
         >
           <SearchIcon />
         </IconButton>
